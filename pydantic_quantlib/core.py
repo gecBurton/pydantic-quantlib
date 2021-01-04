b6069ef8a00376bc7f31df86e72d4dfe7b56ef95
gecBurton <g.e.c.burton@gmail.com>
2021-01-04 10:52:52 +0000

    added doctest to CI

--- pydantic_quantlib/core.py
+++ pydantic_quantlib/core.py
@@ -6,18 +6,31 @@ from pydantic import BaseModel as _BaseModel
 
 class BaseModel(_BaseModel):
     class Config:
-        extra = "forbid"
+        extra = "forbid"  # no unexpected fields
 
     def to_quantlib(self, field=None):
-        if field is None:
+        """recover QuantLib object for computation
+        >>> class Date(BaseModel):
+        ...     d: int
+        ...     m: int
+        ...     y: int
+        >>> date = Date(d=1, m=1, y=2021)
+        >>> date.to_quantlib()
+        Date(1,1,2021)
+        """
+
+        if field is None:  # convert the whole object
             ql_obj = getattr(ql, self.__repr_name__())
             args = (self.to_quantlib(field) for field in self.__fields__)
             return ql_obj(*filter(None, args))
 
         attr = getattr(self, field)
         if isinstance(attr, BaseModel):
+            # recursively convert the field values
             return attr.to_quantlib()
         if isinstance(attr, Enum):
+            # extract integer value of Enum
             return attr.value
 
+        # else no conversion required
         return attr
