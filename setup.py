4f77a448cfefef2aacbc4e991c33b357914090d0
gecBurton <g.e.c.burton@gmail.com>
2021-01-05 14:09:31 +0000

    removed <py38

--- setup.py
+++ setup.py
@@ -23,17 +23,15 @@ test_requirements = [
 setup(
     author="George EC Burton",
     author_email="g.e.c.burton@gmail.com",
-    python_requires=">=3.5",
+    python_requires=">=3.8",
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     description="QuantLib Python Objects with Typing",
     install_requires=requirements,
@@ -47,6 +45,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/gecBurton/pydantic-quantlib",
-    version="0.1.1",
+    version="0.1.2",
     zip_safe=False,
 )
